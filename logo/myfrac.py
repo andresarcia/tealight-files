454a173370a1c573b05e81e13df7d298960df798
Andrés Arcia-Moret <andres.arcia@gmail.com>
2015-08-17 22:55:43 +0100

    Running logo/myfrac.py

--- logo/myfrac.py
+++ logo/myfrac.py
@@ -9,8 +9,8 @@ def segment(scale, detail):
      turn(120)
      segment(scale, detail - 1)
      turn(120)
-     segment(scale / 4.0, detail - 1)
-     segment(scale / 4.0, detail - 1)
+     segment(scale / 2.0, detail - 1)
+     segment(scale / 2.0, detail - 1)
      turn(-120)
      
     
