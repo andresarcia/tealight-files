1df80f1b82712372beecf792979577ea8c289036
Andrés Arcia-Moret <andres.arcia@gmail.com>
2015-08-18 00:33:29 +0100

    Running logo/myfrac.py

--- logo/myfrac.py
+++ logo/myfrac.py
@@ -27,7 +27,7 @@ def segment(scale, n,pos):
        move(-scale/2)
        turn(-60)
        move(-scale/2)
-       turn(60)
+       turn(120)
            
      segment(scale/2, n - 1, 0)
      segment(scale/2, n - 1, 1)
