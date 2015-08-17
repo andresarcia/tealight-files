1a3ea3040834fcd2e59edfbed5d58ebb627f5091
Andrés Arcia-Moret <andres.arcia@gmail.com>
2015-08-18 00:08:53 +0100

    Running logo/myfrac.py

--- logo/myfrac.py
+++ logo/myfrac.py
@@ -1,48 +1,25 @@
 from tealight.logo import move, turn
 
+def tri(size):
+     move(-size)
+     turn(120)
+     move(-size)
+     turn(120)
+     move(-size)
+     turn(120)
+
 def segment(scale, n):
   
   if n <= 0:
      return
   else:
+     tri(scale)
      segment(scale, n - 1)
-     move(-scale)
-     turn(120)
-     move(-scale)
-     turn(120)
-     move(-scale)
-     turn(120)
-     #segment(scale/2.0, n - 2)
-     #move(-scale/2.0)
-     #segment(scale/2.0, n - 2)
  
-#     segment(scale / 2.0, detail - 1)
-#     turn(-120)
-#     segment(scale / 2.0, detail - 1)
-#     turn(-120) 
-#     segment(scale / 2.0, detail - 1)
-#     turn(120)
-#     segment(scale / 2.0, detail - 1)
-#     turn(-120)
-     
+
     
 
 turn(-90)
 move(150)
 segment(300,4)
 #move(-300)
-
-
- #   segment(scale, detail - 1)
-  #  turn(120)
-   # segment(scale / 2.0, detail - 1)
-    #turn(120)
-#    segment(scale / 2.0, detail - 1)
- #   turn(-120)
-  #  segment(scale / 2.0, detail - 1)
-   # turn(-120)
-    #segment(scale / 2.0, detail - 1)
-#    turn(120)
- #   segment(scale / 2.0, detail - 1)
-  #  turn(120)
-   # segment(scale, detail - 1)
