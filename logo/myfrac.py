afe600e56acebbdda156ef35f296075d370c85f4
Andrés Arcia-Moret <andres.arcia@gmail.com>
2015-08-17 22:03:12 +0100

    Running logo/myfrac.py

--- logo/myfrac.py
+++ logo/myfrac.py
@@ -5,7 +5,7 @@ def segment(scale, detail):
   if detail == 0:
     move(-scale)
   else:
-    segment(scale / 2.0, detail - 1)
+    segment(scale, detail - 1)
     turn(120)
     segment(scale / 2.0, detail - 1)
     turn(120)
@@ -24,3 +24,18 @@ turn(150)
 #move(-100)
 segment(200,2)
 #move(-300)
+
+
+ #   segment(scale, detail - 1)
+  #  turn(120)
+   # segment(scale / 2.0, detail - 1)
+    #turn(120)
+#    segment(scale / 2.0, detail - 1)
+ #   turn(-120)
+  #  segment(scale / 2.0, detail - 1)
+   # turn(-120)
+    #segment(scale / 2.0, detail - 1)
+#    turn(120)
+ #   segment(scale / 2.0, detail - 1)
+  #  turn(120)
+   # segment(scale, detail - 1)
