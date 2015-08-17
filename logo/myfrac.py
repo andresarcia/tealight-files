622fdd11abd84e274e1ccdbccbcb1fd61a9358a1
Andrés Arcia-Moret <andres.arcia@gmail.com>
2015-08-17 18:15:40 +0100

    Running logo/myfrac.py

--- logo/myfrac.py
+++ logo/myfrac.py
@@ -1,1 +1,18 @@
-print "A new file!"
+def segment(scale, detail):
+  
+  if detail == 0:
+    move(scale)
+  else:
+    segment(scale / 3.0, detail - 1)
+    turn(-60)
+    segment(scale / 3.0, detail - 1)
+    turn(120)
+    segment(scale / 3.0, detail - 1)
+    turn(-60)
+    segment(scale / 3.0, detail - 1)
+    
+
+turn(90)
+move(-300)
+segment(600,6)
+move(-300)
