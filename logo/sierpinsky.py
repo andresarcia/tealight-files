ebe2cfcd7bf7f0526e37d9dadd84a22e2e72fbbf
Andrés Arcia-Moret <andres.arcia@gmail.com>
2015-08-24 12:10:38 +0100

    Running logo/sierpinsky.py

--- logo/sierpinsky.py
+++ logo/sierpinsky.py
@@ -6,15 +6,18 @@ def sierpinsky (scale, detail):
     move(scale)
     turn(120)
     sierpinsky(scale/2, detail-1)
+    sleep(0.5)
   if (detail>0) :
     move(scale)
     turn(120)
     sierpinsky(scale/2, detail-1)
+    sleep(0.5)
   if (detail>0) :
     move(scale)
     turn(120)
     sierpinsky(scale/2, detail-1)
-    
+    sleep(0.5)
+
     
 turn(90)
 sierpinsky(300,5)
