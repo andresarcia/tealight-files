7e73f093fe08c9faa55ace5004e824a493410f9a
Andrés Arcia-Moret <andres.arcia@gmail.com>
2015-08-24 12:08:08 +0100

    Running logo/sierpinsky.py

--- logo/sierpinsky.py
+++ logo/sierpinsky.py
@@ -1,1 +1,20 @@
-print "A new file!"
+from tealight
+
+
+def sierpisky (scale, detail):
+  if (detail>0) :
+    move(scale)
+    turn(120)
+    sierpinsky(scale/2, detail-1)
+  if (detail>0) :
+    move(scale)
+    turn(120)
+    sierpinsky(scale/2, detail-1)
+  if (detail>0) :
+    move(scale)
+    turn(120)
+    sierpinsky(scale/2, detail-1)
+    
+    
+turn(90)
+sierpinsky(200)
