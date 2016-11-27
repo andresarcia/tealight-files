f523a8d76e38da2353a2d8a88f9cfa6a8382a2b2
Andrés Arcia-Moret <andres.arcia@gmail.com>
2016-11-27 22:23:24 +0000

    Running logo/logo-demo.py

--- logo/logo-demo.py
+++ logo/logo-demo.py
@@ -6,8 +6,8 @@ print "This is tealight!"
 
 colors = ["red", "black", "blue"]
 
-for i in range(1,150,0.5):
-  move(i)
+for i in range(1,150,1):
+  move(i/2)
   turn(35)
   c = colors[i%3]
   color(c)
